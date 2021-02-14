a20b8e58c347c153111e5017695bbb94d3b08b89
Emmanuel-Services <68870605+Emmanuel-Services@users.noreply.github.com>
2021-02-15 00:35:26 +0100

    Update header.php

--- header.php
+++ header.php
@@ -4,18 +4,7 @@
 			<div class="row">
 				<div class="col-md-4 logo text-left">
 					<a class="navbar-brand" href="index.php">
-						<i class="fab fa-linode"></i> BLOG SITE</a>
-				</div>
-				<div class="col-md-4 top-forms text-center mt-lg-3 mt-md-1 mt-0">
-					<span>Welcome Back!</span>
-					<span class="mx-lg-4 mx-md-2  mx-1">
-						<a href="blogadmin/index.php">
-							<i class="fas fa-lock"></i> <strong>Sign In</strong></a>
-					</span>
-					<span>
-						<a href="blogadmin/membership_signup.php">
-							<i class="far fa-user"></i> <strong>Register</strong></a>
-					</span>
+						<i class="fab fa-linode"></i><?php getwebname("titles") ?></a>
 				</div>
 				<div class="col-md-4 log-icons text-right">
 
