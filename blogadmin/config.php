c93fecd01da3bacbc8c59de3ae55c383916d8ad3
Emmanuel-Services <68870605+Emmanuel-Services@users.noreply.github.com>
2021-02-14 21:46:43 +0100

    Add files via upload

---
+++ blogadmin/config.php
@@ -0,0 +1,36 @@
+<?php
+	$dbServer = 'localhost';
+	$dbUsername = 'root';
+	$dbPassword = '';
+	$dbDatabase = 'blog_admin_db';
+
+	$adminConfig = array(
+		'adminUsername' => "admin",
+		'adminPassword' => "204238f5873f750a5c92d377a00bdae2",
+		'notifyAdminNewMembers' => "2",
+		'defaultSignUp' => "1",
+		'anonymousGroup' => "anonymous",
+		'anonymousMember' => "guest",
+		'groupsPerPage' => "10",
+		'membersPerPage' => "10",
+		'recordsPerPage' => "10",
+		'custom1' => "Full Name",
+		'custom2' => "Address",
+		'custom3' => "City",
+		'custom4' => "Phone",
+		'MySQLDateFormat' => "%m/%d/%Y",
+		'PHPDateFormat' => "n/j/Y",
+		'PHPDateTimeFormat' => "m/d/Y, h:i a",
+		'senderName' => "Membership management from Emmanrl",
+		'senderEmail' => "emmanrlblog@gmail.com",
+		'approvalSubject' => "Your membership is now approved, remember that we love you and please do invite your friends and family.",
+		'approvalMessage' => "Dear member,\r\n\r\nYour membership is now approved by the admin. You can log in to your account here:\r\nhttp://localhost/blogadmin\r\n\r\nRegards,\r\nEmmanuel Oladimeji from Emmanrl Blog.",
+		'hide_twitter_feed' => "1",
+		'maintenance_mode_message' => "<b>Our website is currently down for maintenance</b><br>\r\nWe expect to be back in a couple hours. Thanks for your patience.",
+		'mail_function' => "mail",
+		'smtp_server' => "",
+		'smtp_encryption' => "",
+		'smtp_port' => "25",
+		'smtp_user' => "",
+		'smtp_pass' => ""
+	);
